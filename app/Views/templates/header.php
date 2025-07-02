878e330cb5259b419c4e565e6f39a340ca29f172
Alif Nurhidayat <131512446+KillerKing93@users.noreply.github.com>
2025-07-02 13:08:06 +0700

    explicitly tell header to load favicon from public

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= esc($title) ?> | SEA Catering</title>
+    <pre>
+        <link rel="icon" href="<?php echo base_url(); ?>favicon.ico" type="image/x-icon">
+    </pre>
     <!-- CSS & JS Libraries -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
